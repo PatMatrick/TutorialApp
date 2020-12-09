fddbcc48d04bc2084595020feee298ad8be27cb8
matthewpatrick <mpatrick9376@gmail.com>
2020-12-08 22:46:10 -0600

    another

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text is changed boyzz.')} />
+      <Button title="Change Text" onPress={() => setOutputText('The text is changed boyzzz.')} />
       <StatusBar style="auto" />
     </View>
   );
